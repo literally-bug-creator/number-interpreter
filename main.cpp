ee0c887f66fd16ad415ca756f215552afa7a8b7f
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-07 10:11:13 +0300

    Add base `main.cpp` functionality

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
 
+#include "interpreter.hpp"
 
-int main() { return 0; }
+using namespace number_interpreter;
+
+int main() {
+    Interpreter interpreter = Interpreter();
+    std::string str;
+    std::cin >> str;
+
+    std::cout << interpreter.interpret( str ) << std::endl;
+    return 0;
+}
