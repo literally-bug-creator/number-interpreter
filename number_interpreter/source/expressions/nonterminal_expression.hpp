927bd74c2ae4bfbe05674dbad1533fc462a89c2f
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-04 02:11:06 +0300

    Remove `nonterminal` & `terminal` expressions headers

--- number_interpreter/source/expressions/nonterminal_expression.hpp
+++
@@ -1,5 +0,0 @@
-#include "abstract_expression.hpp"
-
-namespace number_interpreter {
-class NonterminalExpression : public AbstractExpression {};
-}  // namespace number_interpreter
--- number_interpreter/source/expressions/terminal_expression.hpp
+++
@@ -1,5 +0,0 @@
-#include "abstract_expression.hpp"
-
-namespace number_interpreter {
-class TerminalExpression : public AbstractExpression {};
-}  // namespace number_interpreter
