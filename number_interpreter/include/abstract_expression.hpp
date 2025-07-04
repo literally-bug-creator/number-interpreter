0acfa650cd3de40a4bba3a827fecbcaff7381234
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-04 12:45:30 +0300

    Refactor `AbstractExpression`, add base constructor

--- number_interpreter/include/abstract_expression.hpp
+++ number_interpreter/include/abstract_expression.hpp
@@ -1,14 +1,16 @@
 #pragma once
+
 #include "context.hpp"
 
 namespace number_interpreter {
 class AbstractExpression {
   public:
-    [[nodiscard]] virtual string interpret(Context& context) const;
     AbstractExpression(const AbstractExpression&) = default;
     AbstractExpression(AbstractExpression&&) = delete;
     AbstractExpression& operator=(const AbstractExpression&) = default;
     AbstractExpression& operator=(AbstractExpression&&) = delete;
+    AbstractExpression();
     virtual ~AbstractExpression() = default;
+    [[nodiscard]] virtual string interpret(Context& context) const;
 };
 }  // namespace number_interpreter
