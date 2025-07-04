40ae0573bc839642b5af83a91f70210375ac6240
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-04 17:35:02 +0300

    Update `Token` implementation

--- number_interpreter/source/token.cpp
+++ number_interpreter/source/token.cpp
@@ -7,11 +7,15 @@ using std::string;
 namespace number_interpreter {
 Token::Token(string str) : value_(std::move(str)) {}
 
-bool Token::isEmpty() {
+bool Token::isEmpty() const {
     return value_.empty();
 }
 
-string Token::getValue() {
+string Token::getValue() const {
     return value_;
+}
+
+Token Token::merge(const Token& other) const {
+    return Token(value_ + other.getValue());
 }
 }  // namespace number_interpreter
