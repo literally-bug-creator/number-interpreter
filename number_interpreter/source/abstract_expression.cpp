55ae545cae29388982114187d4d636864c4e598b
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-04 18:46:39 +0300

    Remove implementation of `AbstractExpression`

--- number_interpreter/source/abstract_expression.cpp
+++
@@ -1,9 +0,0 @@
-#include "abstract_expression.hpp"
-
-namespace number_interpreter {
-AbstractExpression::AbstractExpression() = default;
-
-Token AbstractExpression::interpret(Context& context) const {
-    return Token("");
-}
-}  // namespace number_interpreter
