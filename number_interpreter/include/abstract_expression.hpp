b60219b0474c0b7dc1184fe680a36bcbb1604152
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-04 18:46:49 +0300

    Update `AbstractExpression`

--- number_interpreter/include/abstract_expression.hpp
+++ number_interpreter/include/abstract_expression.hpp
@@ -22,12 +22,12 @@ class Token {
 
 class AbstractExpression {
   public:
+    AbstractExpression() = default;
     AbstractExpression(const AbstractExpression&) = default;
     AbstractExpression(AbstractExpression&&) = default;
     AbstractExpression& operator=(const AbstractExpression&) = default;
-    AbstractExpression& operator=(AbstractExpression&&) = delete;
-    AbstractExpression();
+    AbstractExpression& operator=(AbstractExpression&&) = default;
     virtual ~AbstractExpression() = default;
-    [[nodiscard]] virtual Token interpret(Context& context) const;
+    [[nodiscard]] virtual Token interpret(Context& context) const = 0;
 };
 }  // namespace number_interpreter
